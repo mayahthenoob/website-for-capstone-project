f442b54ca3e64988454148f038b2c2dfe710245c
mayahthenoob <sherapower2022@gmail.com>
2026-01-28 17:29:54 -0400

    changed

--- Additional stuff/File Uploader/upload.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $file_name = $_FILES['file']['name'];
-    $tmp_name = $_files['file']['tmp_name'];
-    $dile_up_name = time().$file_name;
-    move_uploaded_file($tmp_name, "files/", $file_up_name);
-?>
