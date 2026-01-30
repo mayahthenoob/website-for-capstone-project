a51e0b163abfc0d2b10640ccb3bf15640b110180
mayahthenoob <sherapower2022@gmail.com>
2026-01-30 18:37:59 -0400

    sidebar and calculator

---
+++ Things for Website/Calculator/calculator.js
@@ -0,0 +1,73 @@
+const display = document.getElementById('display');
+let currentInput = '0';
+const MAX_DISPLAY_LENGTH = 14;
+
+
+function updateDisplay(){
+    display.textContent = currentInput;
+
+    if(currentInput.length > MAX_DISPLAY_LENGTH){
+        display.style.fontSize = '2rem';
+    }
+    else if(currentInput.length > 6){
+        display.style.fontSize = '2.5rem';
+    }
+    else{
+        display.style.fontSize = '3.5rem';
+    }
+}
+
+function appendValue(value){
+
+
+    const lastChar = currentInput.slice(-1);
+    const operators = ['/','*','+','-'];
+
+    if (operators.includes(lastChar) && operators.includes(value)) return;
+
+    const parts = currentInput.split(/[+\-*/]/);
+    const lastPart = parts[parts.length - 1];
+
+    if  (value === '.' && lastPart.includes('.')) return;
+
+    if(currentInput === '0' && value !== '.'){
+        currentInput = value;
+    }
+    else if(currentInput === 'Error'){
+        currentInput = value;
+    }
+    else{
+        currentInput += value;
+    }
+
+    updateDisplay()
+}
+
+function clearDisplay(){
+    currentInput = '0'
+    updateDisplay()
+}
+
+function deleteLast(){
+    if(currentInput.length > 1){
+        currentInput = currentInput.slice(0,-1);
+    }
+    else{
+        currentInput = '0'
+    }
+
+    updateDisplay()
+}
+
+function calculate(){
+    try {
+        let result = eval(currentInput);
+        currentInput = String(result)
+    } catch (error) {
+        currentInput = 'Error'
+    }
+
+    updateDisplay()
+}
+
+updateDisplay()
