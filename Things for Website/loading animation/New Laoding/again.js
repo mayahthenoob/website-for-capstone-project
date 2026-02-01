0755cabef76919bf194f9547837555101315de57
mayahthenoob <sherapower2022@gmail.com>
2026-01-31 21:41:55 -0400

    change

---
+++ Things for Website/loading animation/New Laoding/again.js
@@ -0,0 +1,26 @@
+// Fade out the loading page after 3.5 seconds
+gsap.fromTo(
+  ".loading-page",
+  { opacity: 1 },
+  {
+    opacity: 0,
+    display: "none",
+    duration: 1.5,
+    delay: 3.5,
+  }
+);
+
+// Animate the logo-name text
+gsap.fromTo(
+  ".logo-name",
+  {
+    y: 50,
+    opacity: 0,
+  },
+  {
+    y: 0,
+    opacity: 1,
+    duration: 2,
+    delay: 0.5,
+  }
+);
