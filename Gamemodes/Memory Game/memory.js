d53949a7abccf406e38917edcbf268e6eb1acf18
mayahthenoob <sherapower2022@gmail.com>
2026-02-28 18:21:42 -0400

    del

--- Gamemodes/Memory Game/memory.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const grid = document.querySelector(".game_grid");
-    const restartbtn = document.getElementById("restart_btn");
-    const moveCounter = document.getElementById("move_counter");
-    const timer = document.getElementById("timer");
-
-    const symbols = ["", "", "", "", "", ""];
-    let cards 
-})
