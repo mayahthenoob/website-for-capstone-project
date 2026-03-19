042bd7582dfc1ffd851f3e3b354f457a918732a2
mayahthenoob <sherapower2022@gmail.com>
2026-03-18 23:20:09 -0400

    fix sidebar

--- Things for Website/Present/Dashbaord/e.js
+++ Things for Website/Present/Dashbaord/e.js
@@ -1,12 +1,20 @@
 const body = document.querySelector('body'),
       sidebar = body.querySelector('nav'),
       toggle = body.querySelector(".toggle"),
+      toggleIcon = toggle.querySelector('i'),
       modeSwitch = body.querySelector(".toggle-switch"),
       modeText = body.querySelector(".mode-text");
 
 // Toggle sidebar open/close
 toggle.addEventListener("click", () => {
     sidebar.classList.toggle("close");
+
+    // Rotate or switch icon (X -> menu)
+    if(sidebar.classList.contains("close")){
+        toggleIcon.classList.replace("bx-x","bx-menu"); // shows menu icon when closed
+    } else {
+        toggleIcon.classList.replace("bx-menu","bx-x"); // shows X icon when open
+    }
 });
 
 // Dark mode toggle
